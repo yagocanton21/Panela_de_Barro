77c048fc96a3afe430b1ddf9aa17085d106746fd
unknown <yagocanton123@gmail.com>
2026-01-08 15:06:26 -0300

    Dashboard melhorado e correções gerais

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,14 +1,9 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // Mock do axios
 jest.mock('axios');
 
 test('renders app without crashing', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  render(<App />);
 });
